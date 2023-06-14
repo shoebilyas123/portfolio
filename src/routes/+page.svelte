f47f6522dae5e4130ba0d2f9c9c392e68e546fa5
shoebilyas123 <shoebilyas123@gmail.com>
2023-06-14 20:40:33 +0530

    feat(routes/page.svelte): typewriter effect on skills text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,41 @@
 	import Icon from '@iconify/svelte';
 	import { socialsData } from '../Store';
 	import { fade, scale } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
+	export let skillsHighlight = '';
+	export const skillsText = 'MERN | DevOps | Open Source';
+	let typingIndex = 0;
+	let typingInterval = null;
+
+	const startTyping = () => {
+		if (typingIndex < skillsText.length) {
+			skillsHighlight += skillsText[typingIndex];
+			typingIndex++;
+		} else {
+			stopTyping();
+		}
+	};
+
+	const startTypingInterval = () => (typingInterval = setInterval(startTyping, 50));
+
+	const stopTyping = () => {
+		clearInterval(typingInterval);
+		typingIndex = 0;
+	};
+
+	onMount(() => {
+		startTypingInterval();
+	});
 </script>
 
-<div
-	class="border border-red-400 flex items-center justify-center min-h-screen min-w-screen space-x-60"
->
+<div class=" flex items-center justify-center min-h-screen min-w-screen space-x-60">
 	<div class="flex flex-col space-y-4">
 		{#each $socialsData.list as { icon, href }, i}
 			<div
 				in:scale={{
 					delay: 150 * i
 				}}
-				class="border border-red-400"
 			>
 				<a {href} target="_blank">
 					<Icon {icon} {...$socialsData.commonProps} />
@@ -22,14 +45,16 @@
 		{/each}
 	</div>
 	<div>
-		<img src="/branding_image.png" alt="Shoeb Ilyas" id="branding_image" width="450em" />
-		<div>
+		<img src="/branding_image.png" alt="Shoeb Ilyas" id="branding_image" width="550em" />
+		<!-- <div>
 			<button>Let's Connect</button>
-		</div>
-		<div in:fade>
+		</div> -->
+		<div in:fade class="flex flex-col items-start">
 			<p>Hello! My name is</p>
-			<h1>SHOEB ILYAS</h1>
-			<span>MERN | DevOps | Open Source</span>
+			<div class="flex flex-col items-center justify-center">
+				<h1 class="text-8xl font-bold">SHOEB ILYAS</h1>
+				<span>{skillsHighlight}</span>
+			</div>
 		</div>
 	</div>
 	<div>
